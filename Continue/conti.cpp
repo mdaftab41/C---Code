15951703400c47af24ce13c01a69a92d65f3e022
mdaftab41 <mdaftabalam@gmail.com>
2023-10-14 05:35:10 +0530

    first commit

---
+++ Continue/conti.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+int main(){
+  
+    for(int i= 0  ; i <= 15 ;  i++ )
+    {  
+    cout<<"hello" <<endl;
+    cout <<"hey" <<endl ;
+    continue;
+    cout <<"ye print nahi hoga due to continue ";
+    }
+return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+int main(){
+  
+    for(int i= 0  ; i <= 15 ;  i++ )
+    { if(1&i){
+      cout <<i <<"" <<endl;
+    
+    }
+    cout<<"hi";
+    
+}
+return 0;
+}
