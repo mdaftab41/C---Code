14fed2dda7d60f7ff4bb5e9d1b91ecc42de80afd
mdaftab41 <mdaftabalam49041@gmail.com>
2024-06-12 00:03:59 +0530

    quickShort

---
+++ Recursion/QuickSort.cpp
@@ -0,0 +1,19 @@
+# include<iostream>
+using namespace std;
+int quickSort( int arr[] , int s , int e){
+    if(s<=e){
+        return ;
+    }
+    
+}
+
+int main(){
+    int arr[10] ={9,8,3,18,13,2,8,12,1,0};
+    int n=10;
+    int s= 0;
+    for(int i=0 ; i<10 ; i++){
+        cout<<arr[i];
+        }
+        cout<<endl;
+
+}
