59b37ad6b19ff3f0de9422722bd24c17bf32a9ca
mdaftab41 <mdaftabalam49041@gmail.com>
2024-06-19 17:57:05 +0530

    Static Variable

---
+++ OOPS/Static-keyword.cpp
@@ -0,0 +1,36 @@
+# include <iostream>
+using namespace std;
+
+ class student {
+    string name ;// we use getter and setter to access private member 
+    public: 
+    static int timeToComplete;
+    int rollNo;
+    string subject ;
+    int Marks ;
+    void print (){
+       cout<< name<<endl ; 
+       cout<< rollNo<<endl<<subject<<endl <<Marks<<endl ; 
+    }
+    student( string name , int rollNo , string subject , int Marks){
+        this->name=name;
+        this->Marks=Marks;
+        this->rollNo=rollNo;
+        this->subject=subject;
+    }
+};
+int student :: timeToComplete =10;
+
+
+ int main( ) {
+    student temp("Md Aftab alam" , 18 , "DSA" , 95);
+
+    temp.print();
+   cout<<"Accessing static Variable"<<endl;
+    cout<<student::timeToComplete<<endl;
+    cout<<"Accessing static Variable using object"<<endl;
+   cout<< temp.timeToComplete<<endl;
+    
+    return 0;
+}
+ 
