aa9f65251e923b038e91cb835fbba1450f6ae09a
mdaftab41 <mdaftabalam49041@gmail.com>
2024-06-20 03:51:27 +0530

    protected modifier

---
+++ OOPS/inheritence-protected.cpp
@@ -0,0 +1,46 @@
+# include <iostream> 
+ using namespace std;
+
+ class human{
+    protected:
+    int weight;
+    int height;
+    string name;
+
+    // void setter(int weight , int height , string name){
+    //     this->height = height;
+    //     this->weight=weight;
+    //     this->name= name ;
+    // }
+
+    // void getter(){
+    //     cout<<" Name is : "<<name <<endl <<"Height is : " <<height <<endl <<" Weight is :" <<weight<<endl ;
+    // }
+ };
+// protected is similar to private but  only accessible within class and child class
+ class male : protected human{
+    public:
+    string state;
+    void sleep(){
+        cout<<"sleeping now"<<endl;
+    }
+    void setter(int weight , int height , string name){
+        this->height = height;
+        this->weight=weight;
+        this->name= name ;
+    }
+
+    void getter(){
+        cout<<" Name is : "<<name <<endl <<"Height is : " <<height <<endl <<" Weight is :" <<weight<<endl ;
+    }
+ };
+
+ int main(){
+   male obj1;
+   obj1.setter(45 ,5,"Aftab Alam");
+   obj1.getter();
+   obj1.state="Bihar";
+   cout<<" State is : " << obj1.state <<endl;
+   obj1.sleep();
+    return 0;
+ }
