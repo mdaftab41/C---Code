d2278c2bdb70ed84b5ab2a9aa7c173e623ce0866
mdaftab41 <mdaftabalam49041@gmail.com>
2024-06-21 10:19:24 +0530

    deletion

---
+++ LinkList/singly/deletion.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data ;
+    Node* next;
+    // constructor
+    Node(int data){
+        this->data=data;
+        this->next= NULL;
+    }
+    //destructor
+    ~Node() {
+        int value = this -> data;
+        //memory free
+        if(this->next != NULL) {
+            delete next;
+            this->next = NULL;
+        }
+        cout << " memory is free for node with data " << value << endl;
+    }
+
+};
+
+//function to insert the node at first position 
+
+void InsertionAtFirst(Node*&head,int data){
+    
+    Node* newNode=new Node(data);
+    newNode->next=head;
+    head=newNode;
+    
+}
+
+
+//function to insert the node at any position 
+
+void InsertionAtPos( Node*&head , int pos , int data){
+  Node* posNode = new Node(data);
+  Node* temp= head;
+  if(pos==1){
+    temp->next=head;
+    head=temp;
+    return ;
+  }
+  int cnt =1;
+  while(cnt != pos-1 && temp != NULL){
+    temp=temp->next;
+    cnt++;
+
+  }
+  if(temp==NULL){
+    cout << "Position is out of bounds to add node" << endl;
+        delete posNode; // Clean up memory if position is invalid
+        return;
+  }
+   posNode->next=temp->next;
+  temp->next =posNode;
+  
+}
+
+
+//function to insert hte node at Last position 
+
+void InsertionAtLast(  Node* &head ,int data ){
+    Node* tail = head ;
+    Node* lastNode= new Node(data);
+     if (head == NULL) {
+        // If the list is empty, the new node becomes the head
+        head = lastNode;
+    }
+    while(tail->next!=NULL){
+        tail=tail->next;
+    }
+   tail->next=lastNode;
+   
+}
+
+// functiont to delete the node
+
+void deleteNode(Node*&head , int pos){
+    Node* current =head;
+    if (head == NULL) {
+        cout << "List is empty." << endl;
+        return;
+    }
+    if(pos == 1){
+      head=head->next;
+      current->next=NULL;
+      delete current ;
+      return ;
+    }
+    else{
+         Node* pre=head;
+        while (pos > 1 && current!=NULL)
+        {
+            pre =current;
+            current=current->next;
+            pos--;
+        }
+        if(current==NULL){
+            cout << "Position is out of bounds to delete" << endl;
+            return;
+        }
+        pre->next=current->next;
+        current->next=NULL;
+        delete current;
+    }
+
+}
+
+// function to print hte node or travel the node node
+
+void print(Node* &head){ 
+    while(head!=NULL){
+        cout<<head->data <<" ";
+        head=head->next;
+    }
+
+}
+
+int main(){
+    Node* node1 = new Node(10);
+    Node *head=node1;
+    InsertionAtFirst(head,20);
+    InsertionAtFirst(head,30);
+    InsertionAtFirst(head,40);
+    InsertionAtFirst(head,50);
+    InsertionAtLast(head , 60);
+    InsertionAtLast(head , 70);
+    InsertionAtLast(head , 80);
+    InsertionAtPos(head ,11 ,90);
+    cout<<"Delete function Start now"<<endl;
+      deleteNode(head, 1);
+        deleteNode(head, 3);
+    deleteNode(head, 10);
+
+    print(head);
+
+
+
+    return 0;
+}
