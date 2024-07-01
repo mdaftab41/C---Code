d4a921b903d087d01cb57aa4a7896591f65cbc67
mdaftab41 <mdaftabalam49041@gmail.com>
2024-07-01 23:22:55 +0530

    class

--- Stack/ArrayImplementation.cpp
+++ Stack/ArrayImplementation.cpp
@@ -1,2 +1,19 @@
 # include <iostream>
 using namespace std ;
+
+class stack{
+    public:
+  int *arr;
+  int top ;
+  int size ;
+
+  // constructer 
+  stack(int size ){
+  this->size=size ;
+  arr = new int[size];//Dynamic allocation of array 
+  }
+}
+
+int main(){
+
+}
