d18c3416c6f7429b85a601aae8ebc7d162516a65
mdaftab41 <mdaftabalam49041@gmail.com>
2024-07-08 10:48:21 +0530

    vector

--- Vector/basic.cpp
+++ Vector/basic.cpp
@@ -10,10 +10,11 @@ int main(){
     for( int i = 0 ; i< n ; i++){
         cin>>v[i] ;
     }
-
+  cout<<"vector using the auto loop "<< endl ;
     for(auto it =v.begin() ; it <v.end(); it++){
         cout<<*it<<endl ;
     }
+    cout<<" vector using for loop"<<endl ;
     for(int i = 0 ; i<n ; i++){
         cout<<v[i]<<" ";
     }
