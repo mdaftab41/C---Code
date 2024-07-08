5d672fdf835cba4285820a4775fd6c79a41289fc
mdaftab41 <mdaftabalam49041@gmail.com>
2024-07-08 13:31:18 +0530

    autoLoop

---
+++ Vector/vector-1.cpp
@@ -0,0 +1,28 @@
+# include <iostream>
+# include <vector>
+using namespace std; 
+
+int main(){
+    vector<int> v ;
+    v.push_back(5) ;
+    v.push_back(80) ;
+    v.push_back(70) ;
+    v[0] =10 ;
+    v.insert(v.begin()+1 ,50);
+    cout<<v[1] <<endl;
+    v.erase(v.begin()+1 );
+    cout<<v[1] <<endl;
+    cout<<v[0]<<endl ;
+    v.push_back(6) ;
+    v.push_back(30) ;
+    v.push_back(20) ;
+    v.push_back(10) ;
+    cout<<"Display the fornt and Back Element : "<<endl ;
+    cout<<"front Element : " <<v.front() <<endl ;
+    cout<<"Back Element : " <<v.back() <<endl ;
+    cout<<"now we using the auto loop to display vector"<<endl ;
+    for(auto i : v){
+        cout<< i<< " " ;
+    }
+    return 0 ;
+}
