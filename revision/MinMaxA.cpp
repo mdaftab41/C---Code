4b10642940fdc3ae9df4c7e590f211ead2753ce9
mdaftab41 <mdaftabalam49041@gmail.com>
2024-07-09 23:36:55 +0530

    maxiElement

---
+++ revision/MinMaxA.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int getMax( int arr[] , int size){
+    int maxi = INT_MIN ;
+    for( int i = 0 ; i < size ; i++){
+        if(maxi < arr[i]){
+            maxi = arr[i] ;
+        }
+    }
+    return maxi ;
+}
+
+void printArr(int arr[] , int size){
+    for( int i= 0 ; i< size ; i++){
+        cout<<arr[i] <<"  " ;
+    }cout<<endl ;
+}
+
+int main(){
+int arr[6] ={5,6,7,45,3,8} ;
+printArr( arr , 6);
+int maxiElement = getMax(arr , 6) ;
+cout<<"Maxi Element is :"<<maxiElement <<endl;
+
+return 0;
+}
+
+
+ 
