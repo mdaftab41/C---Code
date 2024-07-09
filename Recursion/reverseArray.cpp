66fcb4fd17d871bc450f25398c06dd3c04a567f2
mdaftab41 <mdaftabalam49041@gmail.com>
2024-07-09 21:00:43 +0530

    reverseArray

---
+++ Recursion/reverseArray.cpp
@@ -0,0 +1,28 @@
+# include<iostream>
+using namespace std ;
+
+void reverse( int arr[] , int size){
+    int s=0 ;
+    int e=size-1;
+    while(s<=e){
+        int temp;
+        temp =arr[s] ;
+        arr[s] = arr[e];
+        arr[e] =temp ;
+        s++;
+        e--;
+    }
+}
+
+int main(){
+    int arr[10] ;
+    for(int i=0 ; i<10 ; i++){
+        cin>>arr[i] ;
+
+    }
+     reverse(arr , 10) ;
+     for(int i =0 ; i<10 ; i++){
+        cout<<arr[i] <<" " ;
+     }
+     return 0 ;
+}
