f1f253d12b60473cea017b972756ae8a7d353e83
mdaftab41 <mdaftabalam49041@gmail.com>
2024-07-10 23:58:42 +0530

    unionArray

---
+++ revision/UnionArrSet.cpp
@@ -0,0 +1,25 @@
+# include <bits/stdc++.h>
+using namespace std ;
+
+void arrUnion(int arr1[] , int arr2[] , int n , int m){
+    set<int> s;
+    for( int i= 0 ; i<n ; i++){
+        s.insert(arr1[i]);
+    }
+   for( int i=0; i<m ; i++){
+    s.insert(arr2[i]);
+   }
+    for (auto itr = s.begin(); itr != s.end(); itr++)
+        cout << *itr << " ";
+}
+ 
+
+int main(){
+
+     int arr1[] = {2,4,6,7};
+     int arr2[] ={8,9,0,2,5,6,5,4};
+     int m=8;
+     int n=4;
+     arrUnion(arr1 ,arr2 , n , m) ;
+    return 0;
+}
