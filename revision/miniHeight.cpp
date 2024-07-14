eef86bd65f502a2b01a193a14852e157de7f7330
mdaftab41 <mdaftabalam49041@gmail.com>
2024-07-14 17:33:50 +0530

    minHeight

--- revision/miniHeight.cpp
+++ revision/miniHeight.cpp
@@ -6,7 +6,7 @@ void miniHeight( int arr[] , int n , int k){
       int mini=arr[0]-k;
       int maxi=arr[n-1]+k;
       int ans=arr[n-1] - arr[0] ;
-     for( int i=0 ; i< n ; i++ ){
+     for( int i=1 ; i< n ; i++ ){
          
         
         if((arr[i]-k) <=0){
