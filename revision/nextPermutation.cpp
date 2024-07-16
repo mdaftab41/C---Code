ee08d1f8a4c0c1e55b59a25e1bcde4805d84b1bb
mdaftab41 <mdaftabalam49041@gmail.com>
2024-07-16 23:25:05 +0530

    nextPermutation

---
+++ revision/nextPermutation.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> nextPermutation(vector<int>& arr) {
+    int N = arr.size();
+    int i;
+    for (i = N - 2; i >= 0; --i) {
+        if (arr[i] < arr[i + 1]) break;
+    }
+    int j;
+    if (i >= 0) {
+        for (j = N - 1; j > i; --j) {
+            if (arr[j] > arr[i]) break;
+        }
+        swap(arr[i], arr[j]);
+    }
+    reverse(arr.begin() + i + 1, arr.end());
+
+    return arr;
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 6, 5, 4};
+
+    vector<int> nextPerm = nextPermutation(arr);
+
+    cout << "Next permutation: ";
+    for (int num : nextPerm) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
