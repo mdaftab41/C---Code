4669b96ca9c20b1ccc1bb51f192e465d67bce851
mdaftab41 <mdaftabalam49041@gmail.com>
2024-07-17 17:57:43 +0530

    MaxiOccuranceCharacter

---
+++ revision/MaxiOccurOfChar.cpp
@@ -0,0 +1,39 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+char MaxOccChara(string s){
+    int arr[26]={0};// every element of array is 0
+    // creating array of counting of char 
+   
+    for(int i=0; i<s.length() ; i++){
+        
+        char ch =s[i];
+            int number=0;
+        if(ch>='a' && ch<='z'){
+            
+            number = ch - 'a';
+        }
+        else {
+              number = ch-'A';
+        }
+         arr[number]++;
+    }
+
+    int maxi= -1, ans=0;
+    for( int i=0 ; i<26 ; i++){
+        if( maxi <= arr[i]){
+            ans= i;
+            maxi=arr[i] ;
+        }
+    }
+    char final='a' + ans;
+    return final;
+    
+}
+
+int main(){
+    string s="aftabalam";
+    cout<<MaxOccChara(s)<<endl;
+
+    return 0;
+}
