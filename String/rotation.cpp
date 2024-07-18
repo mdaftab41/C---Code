6aa5cabf650af3bea5025b7d4a0cdaff909ec9d4
mdaftab41 <mdaftabalam49041@gmail.com>
2024-07-18 23:08:13 +0530

    Rotation

---
+++ String/rotation.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check_rotation(string s, string goal)
+{
+    if (s.size() != goal.size())
+        return false;
+
+    queue<char> q1;
+    for (int i = 0; i < s.size(); i++) {
+        q1.push(s[i]);
+    }
+
+    queue<char> q2;
+    for (int i = 0; i < goal.size(); i++) {
+        q2.push(goal[i]);
+    }
+
+    int k = goal.size();
+    while (k--) {
+        char ch = q2.front();
+        q2.pop();
+        q2.push(ch);
+        if (q2 == q1)
+            return true;
+    }
+    return false;
+}
+
+// Driver code
+int main()
+{
+    string str1 = "AACD", str2 = "ACDA";
+
+    if (check_rotation(str1, str2))
+        printf("Strings are rotations of each other");
+    else
+        printf("Strings are not rotations of each other");
+    return 0;
+}  
