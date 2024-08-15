d639702fe3e5cf6a97f9a63b61c438e773b056d2
mdaftab41 <mdaftabalam49041@gmail.com>
2024-08-15 23:29:33 +0530

    Array

---
+++ 2D-array/basic.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //creating 2D array
+   int arr[3][4];
+
+   for(int i= 0 ; i<3 ; i++){
+    for(int j=0 ; j<4 ;j++){
+        cin>>arr[i][j] ;
+    }
+
+   }
+    for(int i=0 ; i<3 ;i++){
+    for(int j=0 ; j<4 ;j++){
+        cout<<arr[i][j] <<" ";
+    }
+
+   }
+   return 0 ;
+}
