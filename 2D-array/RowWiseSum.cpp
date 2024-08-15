6b30b4971eae4a701bcb4d42a6b0bd2a03b67ba3
mdaftab41 <mdaftabalam49041@gmail.com>
2024-08-16 00:06:23 +0530

    RowWiseSum

--- 2D-array/RowWiseSum.cpp
+++ 2D-array/RowWiseSum.cpp
@@ -3,18 +3,20 @@ using namespace std;
 int main(){
     int arr[4][4] ;
     int sum ;
+    cout<<" printing the element of the array"<<endl ;
     for(int i=0 ; i<4 ; i++){
         for(int j=0 ; j<4 ; j++){
             cin>>arr[i][j] ;
         }
     }
+    cout<<"Printing the sum "<<endl ;
     for(int i=0 ; i<4 ; i++){
         for(int j=0 ; j<4 ; j++){
             cout<<arr[i][j] <<" ";
         }
         cout<<endl ;
     }
-    cout<<"Row Wise sum "<<endl;
+    cout<<"rpinting Row Wise sum "<<endl;
     for(int i=0 ; i<4 ; i++){
         sum =0 ;
         for(int j=0 ; j<4 ; j++){
