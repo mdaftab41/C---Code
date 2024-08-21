430a22aecea5fba0c93a3f973e1f7f0535222220
mdaftab41 <mdaftabalam49041@gmail.com>
2024-08-21 16:57:37 +0530

    compress the string

--- String/sringCompression.cpp
+++ String/sringCompression.cpp
@@ -1,36 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+// function for compress string
+void compress(vector<char>&chars){
+  int i=0;
+  int ansIndex=0;
+  int n=chars.size();
+  while(i<n){
+    int j=i+i;
+    while(j<n &&chars[i]==chars[j]){
+        j++;
+    }
+    chars[ansIndex++]=chars[i];
+    cout<<"printing Index  " <<ansIndex<<endl;
+    int count=j-i;
+    if(count>1){
+        string cnt= to_string(count);
+        for(char ch: cnt){
+            chars[ansIndex++]=ch;
+        }
+    }
+    i=j;
+  }
+  cout<<ansIndex<<endl;
+ // return ansIndex;
+}
 
 int main(){
-   // char word[80];  // this will hold 79 letters
-   //vector<car>v (80);
-   // cout << "Please enter a word: ";
-    //cin >> word;
-    // char ch;
-    // for( int i=0 ; i<80;i++){
-    //     cin>>ch;
-    //     v.push_back(ch);
-    // }
-    // for( int i=0 ; i<=v.size(); i++){
-    //      cout<<v[i];
-    // }
-    // return 0 ;
     vector<char> v;  // Create an empty vector to store characters
 
     cout << "Please enter a word: ";
-
     char ch;
     while (cin.get(ch) && ch != '\n') {  // Read each character until newline is encountered
         v.push_back(ch);  // Add the character to the vector
     }
 
-    // Print each character stored in the vector
-    for (int i = 0; i < v.size(); i++) {
-        cout << v[i];
-    }
-
+    // // Print each character stored in the vector
+    // for (int i = 0; i < v.size(); i++) {
+    //     cout << v[i];
+    // }
+    cout<<endl;
+    compress(v);
     return 0;
 }
 
-a
