b31af0cd8750ced908579d87ce8d22627708a2ff
mdaftab41 <mdaftabalam49041@gmail.com>
2024-08-22 16:58:09 +0530

    pointer

--- pointer/basic.cpp
+++ pointer/basic.cpp
@@ -6,5 +6,10 @@ int main(){
     int *ptr=&num ;
     cout<<"Address of p is : "<<ptr<<endl;
     cout<<"value at p is : "<<*ptr<<endl;
+
+    double d=12.6 ;
+    double *dPtr=&d;
+    cout<<"Address of p is : "<<dPtr<<endl;
+    cout<<"value at p is : "<<*dPtr<<endl;
     return 0;
 }
