d0d30240161821b3fa36979e06721f8fef97b680
mdaftab41 <mdaftabalam49041@gmail.com>
2024-08-22 17:04:18 +0530

    sizeof

--- pointer/basic.cpp
+++ pointer/basic.cpp
@@ -11,5 +11,10 @@ int main(){
     double *dPtr=&d;
     cout<<"Address of p is : "<<dPtr<<endl;
     cout<<"value at p is : "<<*dPtr<<endl;
+    cout<<"working on size"<<endl;
+    cout<<"size of integer is : "<<sizeof(num)<<endl;
+    cout<<"size of pointer is : "<<sizeof(ptr)<<endl;
+    cout<<"size of double is : "<<sizeof(d)<<endl;
+    cout<<"size of double-pointer is : "<<sizeof(dPtr)<<endl;
     return 0;
 }
