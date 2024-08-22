85bbce1aa7321238ad8f95afbeb9f66dcb57e9a4
mdaftab41 <mdaftabalam49041@gmail.com>
2024-08-22 12:23:57 +0530

    GCD using Recursion

--- Maths/gcd.cpp
+++ Maths/gcd.cpp
@@ -16,11 +16,23 @@ int gcdFun(int a, int b){
  return a;
 }
 
+int gcdRecusion( int a, int b){
+    if(b!=0)
+      return gcdRecusion(b ,a%b);
+    else
+      return a;
+}
+
 int main(){
     int a , b ;
     cout<<"Enter the number to find Hcf"<<endl;
     cin>>a>>b ;
     int gcd=gcdFun(a,b);
+    int ans=gcdRecusion(a, b);
+    cout<<"HCF using iterative function"<<endl ;
     cout<<"HCF of "<<a<<" , "<<b<<" is : "<<gcd<<endl ;
+
+    cout<<"HCF using iterative function"<<endl ;
+    cout<<"HCF of "<<a<<" , "<<b<<" is : "<<ans<<endl ;
     return 0;
 }
