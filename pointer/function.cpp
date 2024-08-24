ee2ed0510a74804a89f90aec290bc347f0e1badf
mdaftab41 <mdaftabalam49041@gmail.com>
2024-08-24 11:54:25 +0530

    function

--- pointer/function.cpp
+++ pointer/function.cpp
@@ -6,18 +6,28 @@ void print(int *p){
 }
 
 void update(int *p){
-    p=p+1 ;
+   // p=p+1 ;
     //value of address will change here only not in main function
-    cout<<"inside update function : "<<p<<endl ;
+   // cout<<"inside update function : "<<p<<endl ;
+    *p=*p+2;
+    cout<<"in update function : "<<*p<<endl;
+    
 }
 int main(){
     int value=10 ;
     int *p=&value ;
-    print(p) ;
-    cout<<"before update in main function : "<<p<<endl;
+    // print(p) ;
+    // cout<<"before update in main function : "<<p<<endl; 
+    // update(p) ;
+    // //we can't change pointer location
+    // cout<<"after update in main function : "<<p<<endl;
+    // cout<<"we can update the value inside update function : "<<endl;
+
+    cout<<"before updating value in main function : " << *p <<endl;
     update(p) ;
-    //we can't change pointer location
-    cout<<"after update in main function : "<<p<<endl;
+    //we can update the value inside update function 
+    // change will also refelected in main function
+    cout<<"after updating value  in main function : "<< *p <<endl;
 
     return 0 ;
 }
