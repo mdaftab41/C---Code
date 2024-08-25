c75505c04775ff6452de411f74434a4856b1f602
mdaftab41 <mdaftabalam49041@gmail.com>
2024-08-25 18:38:33 +0530

    ASCII Value

---
+++ Striver’s-TCS/StringProblem/findASCII.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char ch ;
+    cout<<"Enter the character "<<endl ;
+    cin>> ch ;
+    int value = int(ch) ;
+    cout<<"ASCII value of "<< ch <<" is : "<<value <<endl;
+    return 0 ;
+}
