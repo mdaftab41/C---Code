90fa90711fa96eba5972d42ed6d2226a66903020
mdaftab41 <mdaftabalam49041@gmail.com>
2024-08-26 00:23:31 +0530

    reverseWord

--- Striver’s-TCS/StringProblem/reverseWord.cpp
+++ Striver’s-TCS/StringProblem/reverseWord.cpp
@@ -31,44 +31,73 @@ using namespace std;
 // }
 
  
-string result(string s)
-{
-    int left = 0;
-    int right = s.length()-1;
+// string result(string s)
+// {
+//     int left = 0;
+//     int right = s.length()-1;
     
+//     string temp="";
+//     string ans="";
+    
+//     //Iterate the string and keep on adding to form a word
+//     //If empty space is encountered then add the current word to the result
+//     while (left <= right) {
+//         char ch= s[left];
+//         if (ch != ' ') {
+//             temp += ch;
+//         } else if (ch == ' ') {
+//             if (ans!="") 
+//             ans = temp + " " + ans;
+//             else
+//              ans = temp;
+//             temp = "";
+//         }
+//         left++;
+//     }
+    
+//     //If not empty string then add to the result(Last word is added)
+//     if (temp!="") {
+//         if (ans!="") 
+//         ans = temp + " " + ans;
+//         else ans = temp;
+//     }
+    
+//     return ans;    
+// }
+
+string wordReverse(string str){
+    // creating index
+    int start=0 ;
+    int end=str.length() ;
+    //creating empty string
     string temp="";
-    string ans="";
-    
-    //Iterate the string and keep on adding to form a word
-    //If empty space is encountered then add the current word to the result
-    while (left <= right) {
-        char ch= s[left];
-        if (ch != ' ') {
-            temp += ch;
-        } else if (ch == ' ') {
-            if (ans!="") 
-            ans = temp + " " + ans;
-            else ans = temp;
-            temp = "";
+    string ans="" ;
+    while(start<=end){
+        char ch=str[start] ;
+        if(ch!=' '){
+            temp+=ch ;
+        }else if(ch==' '){
+            if(ans!="") ans=temp+" "+ans ;
+            else ans=temp ;
+            temp="" ;
         }
-        left++;
+      start++;
     }
-    
-    //If not empty string then add to the result(Last word is added)
-    if (temp!="") {
-        if (ans!="") 
-        ans = temp + " " + ans;
-        else ans = temp;
+    // adding last word
+    if(temp!=" ") {
+    if(ans!="") ans=temp+" "+ans ;
+     else ans=temp ;
     }
-    
-    return ans;    
+    return ans ;
 }
+
 int main()
 {
     string st="TUF is great for interview preparation";
     cout<<"Before reversing words: "<<endl;
     cout<<st<<endl;
     cout<<"After reversing words: "<<endl;
-    cout<<result(st);
+    //cout<<result(st);
+    cout<<wordReverse(st)<<endl;
     return 0;
 }
