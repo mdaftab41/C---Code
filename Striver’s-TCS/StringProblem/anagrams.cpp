99b8ab2c3e0468d4ac7db1fc8adeda80b2885670
mdaftab41 <mdaftabalam49041@gmail.com>
2024-08-28 20:07:14 +0530

    anagram

---
+++ Striver’s-TCS/StringProblem/anagrams.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool anagram( string str ,string s){
+    for(int i=0; i<str.size(); i++){
+        str[i] = towlower(str[i]);
+        s[i] = towlower(s[i]);
+    }
+    sort(str.begin(), str.end()) ;
+    sort(s.begin(), s.end());
+      cout<<s<<"--> "<<str<<endl;
+
+    for(int i=0; i<str.size(); i++){
+        str[i] = towlower(str[i]);
+        s[i] = towlower(s[i]);
+        if(s[i]!=str[i]){
+            return false ;
+        }
+    }
+    cout<<s<<" "<<str<<endl;
+    return true ;
+}
+
+int main(){
+    string str;
+    string s;
+    cout<<"Enter the two string "<<endl;
+    cin>>str>>s ;
+    if(anagram(str ,s)){
+        cout<<"string are anagram of each other "<<endl;
+    }else{
+        cout<<"string are not anagram of each other "<<endl;
+    }
+    return 0;
+}
