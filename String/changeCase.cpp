04bbb90e42efcdad5fcd98ed47b67a8ce51829c6
mdaftab41 <mdaftabalam49041@gmail.com>
2024-08-28 23:36:31 +0530

    change case

---
+++ String/changeCase.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+void solve(string str, int n) {
+
+  for (int i = 0; i < n; i++) {
+    int ascii = (int) str[i];
+
+    if (ascii >= 65 && ascii <= 90) // if uppercase
+      str[i] = towlower(str[i]);
+
+    else if (ascii >= 97 && ascii <= 122) // if lowercase
+      str[i] = towupper(str[i]);
+
+  }
+
+  cout << "Resultant string: " << "\n";
+  cout << str << "\n";
+}
+int main() {
+
+  // Input string
+  string str = "take u forward IS Awesome";
+  int n = str.length();
+
+  solve(str, n);
+  return 0;
+}
