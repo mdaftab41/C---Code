8911b300bb48f5dbe716d3f659d81414cca4c7d6
mdaftab41 <mdaftabalam49041@gmail.com>
2024-08-31 16:19:01 +0530

    smallest Element

---
+++ Striver’s-TCS/ArrayProblem/rotateArrayByK.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void rotateArray( int *arr){
+    int size=arr.sizeof(arr)/sizeof(arr[0]) ;
+    int i=0;
+     for(int i=0 ; i<n; i++){
+        
+     }
+    
+}
+
+int main(){
+
+}
---
+++ Striver’s-TCS/ArrayProblem/smallestElement.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int sortArr(vector<int>& arr) {
+    sort(arr.begin(),arr.end());
+    return arr[0];
+}
+ 
+int main() {
+    vector<int> arr1 = {2,5,1,3,0};
+    vector<int> arr2 = {8,10,5,7,9};
+   
+    cout<<"The smallest element in the array is: "<<sortArr(arr1)<<endl;
+    cout<<"The smallest element in the array is: "<<sortArr(arr2);
+   
+    return 0;
+}
