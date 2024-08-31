a1e2ad6c3c06ba1abcf1c9d866fd999d7e5b4dea
mdaftab41 <mdaftabalam49041@gmail.com>
2024-08-31 16:46:58 +0530

    Rotate Array buy k element

--- Striver’s-TCS/ArrayProblem/rotateArrayByK.cpp
+++ Striver’s-TCS/ArrayProblem/rotateArrayByK.cpp
@@ -2,15 +2,40 @@
 using namespace std;
 
 
-void rotateArray( int *arr){
-    int size=arr.sizeof(arr)/sizeof(arr[0]) ;
-    int i=0;
-     for(int i=0 ; i<n; i++){
-        
-     }
+void reverse(int *arr , int start ,int end){
+    while (start<=end)
+    {
+         swap(arr[start++] , arr[end--]);
+    }
     
 }
 
-int main(){
+void rotateArray( int *arr, int n, int k){
+    //Reverse the first 0 to k-1 element 
+      reverse(arr , 0 , k-1) ;
+      // Reverse the K to n-1 element 
+      reverse(arr ,k ,n-1) ;
+     //Reverse the complete  array 
+     
+      reverse(arr , 0 ,n-1) ;
+    for(int i=0 ; i<n ;i++){
+        cout<<arr[i] <<" " ;
+    }
+    cout<<endl;
+}
 
+int main(){
+    int n;
+    cout<<"Enter the size of array"<<endl;
+    cin>> n ;
+    int arr[n] ;
+    cout<<"Enter the element of Array "<<endl;
+    for(int i=0;i<n ;i++){
+        cin>>arr[i];
+    }
+    int k;
+    cout<<"Enter the number to rotate the Array"<<endl;
+    cin>>k ;
+rotateArray(arr ,n,k) ;
+return 0 ;
 }
