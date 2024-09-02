875df27b5890f86b7b333e32c6805876e6a544a7
mdaftab41 <mdaftabalam49041@gmail.com>
2024-09-02 19:02:21 +0530

    short

--- Array/shortElementByFreq.cpp
+++ Array/shortElementByFreq.cpp
@@ -1,5 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+
+void sort2darray(int array2d[2][100], int k)
+{
+  for (int i = 0; i < k - 1; i++)
+  {
+    for (int j = 0; j < k - 1 - i; j++)
+    {
+      if (array2d[1][j] < array2d[1][j + 1])
+      {
+        swap(array2d[1][j], array2d[1][j + 1]);
+        swap(array2d[0][j], array2d[0][j + 1]);
+      }
+    }
+  }
+}
+// Function to sort elements of array by frequency
+void SortArr(int arr[], int n)
+{
+  // Step 1: Sort the array
+  sort(arr, arr + n);
+  // Step 2: Create a 2d array
+  int arr2d[2][100];
+  int k = 0;
+  int count = 0;
+  for (int i = 0; i < n; i++)
+  {
+    if (i == 0)
+    {
+      arr2d[0][k] = arr[i];
+      count = 1;
+    }
+    else if (arr[i] == arr[i - 1])
+    {
+      count++;
+    }
+    else
+    {
+      arr2d[1][k] = count;
+      count = 1;
+      k++;
+      arr2d[0][k] = arr[i];
+    }
+  }
+  arr2d[1][k] = count;
+  k++;
+ 
+  // Step 3: sort the 2d array according to frequency
+  sort2darray(arr2d, k);
+ 
+  // Step 4: Store the answer in original array
+  k = 0;
+  int i = 0;
+  while (i < n)
+  {
+    while (arr2d[1][k] > 0)
+    {
+      arr[i] = arr2d[0][k];
+      i++;
+      arr2d[1][k]--;
+    }
+    k++;
+  }
+}
+
+
+
+
+
+
+
 bool sortele(pair<int, int> a, pair<int, int> b)
 {
   if (a.second == b.second)
@@ -34,6 +105,7 @@ int main()
   int arr[] = {3, 3, 5, 2, 1, 1, 2};
   int n = 7;
   Sortelementsbyfreq(arr, n);
+  SortArr(arr , n);
   return 0;
 }
  
