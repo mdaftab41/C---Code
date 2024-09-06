1a17e64dbf38df8cb37a75c24f2bd0bb8638ef97
mdaftab41 <mdaftabalam49041@gmail.com>
2024-09-06 23:35:04 +0530

    recursion

--- Striver’s-TCS/Number/factorial.cpp
+++ Striver’s-TCS/Number/factorial.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 using namespace std;
+
+// factorial using Recursion 
+int factorialRec(int n){
+   
+   // Base Condition.
+   if(n == 0)
+   {
+       return 1;
+   }
+
+   // Problem broken down into 2 parts and then combined.
+   return n * factorialRec(n-1);
+
+}
+
+
+
+
 int factorial(int X) {
    int ans = 1;
    for (int i = 1; i <= X; i++) {
@@ -12,5 +30,9 @@ int main() {
    cout<<"Enter the value of x "<<endl;
    cin>>x ;
    int result = factorial(x);
-   cout << "The factorial of "<<x<<" is "  << result;
+   int result1=factorialRec(x);
+   cout << "The factorial of "<<x<<" is "  << result<<endl;
+   cout<<"factorial using recursion"<<endl;
+    cout << "The factorial of "<<x<<" is "  << result1;
+   
 }
