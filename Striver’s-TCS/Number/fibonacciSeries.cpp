23fd7cab4025b10dd53ced8ef2fa97d90da46adc
mdaftab41 <mdaftabalam49041@gmail.com>
2024-09-12 00:23:35 +0530

    febonacci Series

--- Striver’s-TCS/Number/fibonacciSeries.cpp
+++ Striver’s-TCS/Number/fibonacciSeries.cpp
@@ -1,10 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std ;
-
+ // m1
 int fib( int n){
     if(n==0 || n== 1)
-        return 1;
+        return n;
+   // cout<< n<<" ";    
     return fib(n-1)+fib(n-2) ;
+     
+}
+
+void fibonacciIterative(int n) {
+    int fib[n];
+    fib[0] = 0;
+    fib[1] = 1;
+    
+    for (int i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    
+    for (int i = 0; i < n; i++) {
+        cout << fib[i] << " ";
+    }
+    cout << endl;
 }
 
 int main(){
@@ -12,5 +29,7 @@ int n ;
 cout<<"Enter the no"<<endl;
 cin>>n;
 int m=fib(n);
-cout<<"Fabonacci series "<<m<<endl;
+cout<<"Recursive Fabonacci series "<<m<<endl;
+cout<<" Iterative Fabonacci series ";
+fibonacciIterative(n);
 }
