e412016844433783e03871e473106609d7af99ba
mdaftab41 <mdaftabalam49041@gmail.com>
2024-09-12 00:33:44 +0530

    fabonacciSeries

--- Striver’s-TCS/Number/fibonacciSeries.cpp
+++ Striver’s-TCS/Number/fibonacciSeries.cpp
@@ -16,6 +16,7 @@ void fibonacciIterative(int n) {
     
     for (int i = 2; i < n; i++) {
         fib[i] = fib[i - 1] + fib[i - 2];
+        return fib[i];
     }
     
     for (int i = 0; i < n; i++) {
