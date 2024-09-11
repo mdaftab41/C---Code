8d91e2cd73aef146b551f1666d11eae8ac09ea40
mdaftab41 <mdaftabalam49041@gmail.com>
2024-09-12 01:37:12 +0530

    unique no

---
+++ Striver’s-TCS/ArrayProblem/findUnique.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findUnique( int arr[] , int n){
+    int ans=0 ;
+    for( int i=0 ; i< n; i++){
+        ans=ans^arr[i];
+    }
+    return ans ;
+}
+
+
+void print(int arr[] , int n){
+    for( int i=0 ; i<n ; i++){
+    cout<<arr[i]<<" " ;
+}cout<<endl;
+
+ }
+int main(){
+int n ;
+cout<<"Enter the size"<<endl;
+cin>>n ;
+int arr[n] ;
+for( int i=0 ; i<n ; i++){
+    cin>>arr[i];
+}
+print(arr , n) ;
+int unique=findUnique(arr , n) ;
+cout<<"unique no is : "<<unique<<endl;
+    return 0 ;
+}
