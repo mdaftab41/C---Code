492c1d759b6686279a6b30cf0d8d70be7727fa68
mdaftab41 <mdaftabalam49041@gmail.com>
2024-09-26 13:52:52 +0530

    AverageOfPositiveNo

---
+++ revision/average.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+double averageOfPositives(const vector<int>& arr) {
+    int sum = 0;
+    int count = 0;
+
+    for (int num : arr) {
+        if (num > 0) {
+            sum += num;
+            count++;
+        }
+    }
+    if (count == 0) return 0;
+
+    return sum / count +1;
+}
+
+int main() {
+    vector<int> arr = {5, 2, -4, 1, 3};
+    int  average = averageOfPositives(arr);
+    cout << average << endl;
+
+    return 0;
+}
