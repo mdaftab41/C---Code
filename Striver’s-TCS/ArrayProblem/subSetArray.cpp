33d255c691cbeb28ffdab05afdf8e525490c0a5c
mdaftab41 <mdaftabalam49041@gmail.com>
2024-09-30 23:18:24 +0530

    subSetOfArray

--- Striver’s-TCS/ArrayProblem/subSetArray.cpp
+++ Striver’s-TCS/ArrayProblem/subSetArray.cpp
@@ -1,22 +1,51 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+// bool isSubset(int arr1[], int m, int arr2[], int n){
+//     if(m>n) return false;
+//     for(int i=0; i<m; i++){
+// 	    bool present=false;
+// 	    for(int j=0; j<n;j++){
+// 	        if(arr2[j]==arr1[i]){
+// 	            present = true;
+// 	            break;
+// 	        }
+// 	    }
+// 	    if(present==false) return false;
+// 	}
+// 	return true;
+// }
+
+
+
+//optimal solution
+
+bool bSearch(int elem, int arr[], int n){
+    int start=0;
+    int end= n-1;
+    while(start<=end){
+        int mid= (start+end)/2;
+        if(arr[mid]==elem)
+            return true;
+        else if(arr[mid]<elem)
+            start = mid+1;
+        else end= mid-1;
+    }
+    return false;
+}
+
 bool isSubset(int arr1[], int m, int arr2[], int n){
+    
+    sort(arr2,arr2+n); // library function to sort the array
+    
     if(m>n) return false;
     for(int i=0; i<m; i++){
-	    bool present=false;
-	    for(int j=0; j<n;j++){
-	        if(arr2[j]==arr1[i]){
-	            present = true;
-	            break;
-	        }
-	    }
+        
+	    bool present= bSearch(arr1[i],arr2,n);
 	    if(present==false) return false;
 	}
 	return true;
 }
-
-
 int main() {
 	// your code goes here
 	int arr1[]={1,3,4,5,2};
