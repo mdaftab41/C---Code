dba5f5648d4a8f91d6cb1d8708422688c02e6cd4
mdaftab41 <mdaftabalam49041@gmail.com>
2024-09-30 23:20:27 +0530

    SubSetOfArray

--- Striver’s-TCS/ArrayProblem/subSetArray.cpp
+++ Striver’s-TCS/ArrayProblem/subSetArray.cpp
@@ -20,32 +20,53 @@ using namespace std;
 
 //optimal solution
 
-bool bSearch(int elem, int arr[], int n){
-    int start=0;
-    int end= n-1;
-    while(start<=end){
-        int mid= (start+end)/2;
-        if(arr[mid]==elem)
-            return true;
-        else if(arr[mid]<elem)
-            start = mid+1;
-        else end= mid-1;
-    }
-    return false;
-}
+// bool bSearch(int elem, int arr[], int n){
+//     int start=0;
+//     int end= n-1;
+//     while(start<=end){
+//         int mid= (start+end)/2;
+//         if(arr[mid]==elem)
+//             return true;
+//         else if(arr[mid]<elem)
+//             start = mid+1;
+//         else end= mid-1;
+//     }
+//     return false;
+// }
 
-bool isSubset(int arr1[], int m, int arr2[], int n){
+// bool isSubset(int arr1[], int m, int arr2[], int n){
     
-    sort(arr2,arr2+n); // library function to sort the array
+//     sort(arr2,arr2+n); // library function to sort the array
+    
+//     if(m>n) return false;
+//     for(int i=0; i<m; i++){
+        
+// 	    bool present= bSearch(arr1[i],arr2,n);
+// 	    if(present==false) return false;
+// 	}
+// 	return true;
+// }
+
+
+
+ //optimal solution 
+
+ bool isSubset(int arr1[], int m, int arr2[], int n){
     
     if(m>n) return false;
-    for(int i=0; i<m; i++){
-        
-	    bool present= bSearch(arr1[i],arr2,n);
-	    if(present==false) return false;
+    unordered_map<int,bool> mp;
+    for(int i=0; i<n; i++){
+        mp[arr2[i]] = true;
+	}
+	for(int j=0;j<m;j++){
+	    if(mp.count(arr1[j])==0){
+	        return false;
+	    }
 	}
 	return true;
 }
+
+
 int main() {
 	// your code goes here
 	int arr1[]={1,3,4,5,2};
