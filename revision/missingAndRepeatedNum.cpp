87e260f333a459dfd168f0013a05c4731866848c
mdaftab41 <mdaftabalam49041@gmail.com>
2024-10-08 16:18:51 +0530

    missingAndRepeated

--- revision/missingAndRepeatedNum.cpp
+++ revision/missingAndRepeatedNum.cpp
@@ -42,6 +42,30 @@ vector<int> FindMissingAndRepeating(vector<int>&arr){
 //         return { repeated ,missing } ;
 //     }
 
+
+// vector<int> findMissingRepeatingNumbers(vector<int> a) {
+//     int n = a.size(); // size of the array
+//     int hash[n + 1] = {0}; // hash array
+
+//     //update the hash array:
+//     for (int i = 0; i < n; i++) {
+//         hash[a[i]]++;
+//     }
+
+//     //Find the repeating and missing number:
+//     int repeating = -1, missing = -1;
+//     for (int i = 1; i <= n; i++) {
+//         if (hash[i] == 2) repeating = i;
+//         else if (hash[i] == 0) missing = i;
+
+//         if (repeating != -1 && missing != -1)
+//             break;
+//     }
+//     return {repeating, missing};
+// }
+
+
+
 int main(){
     vector<int> arr = {1, 3, 3, 4, 5, 6, 7}; // Example array
     vector<int> result = FindMissingAndRepeating(arr);
