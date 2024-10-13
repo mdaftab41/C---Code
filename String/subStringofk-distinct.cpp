58468ff6f01d1d8592ca6b0c874fe9d6cde6f395
mdaftab41 <mdaftabalam49041@gmail.com>
2024-10-14 01:24:08 +0530

    subString

--- String/subStringofk-distinct.cpp
+++ String/subStringofk-distinct.cpp
@@ -1,38 +1,47 @@
-#include <iostream>
-#include <string>
+ #include <iostream>
 #include <unordered_map>
+#include <string>
 using namespace std;
 
-int calculateFrequency(const string &s, int k) {
-    unordered_map<string, int> substringCount;
-    int n = s.size();
-    
-    // Traverse the string and count all substrings of length k
-    for (int i = 0; i <= n - k; i++) {
-        string sub = s.substr(i, k);
-        substringCount[sub]++;
-    }
-    
-    // Calculate total frequency of unique substrings of length k
-    int uniqueFrequency = 0;
-    for (auto &entry : substringCount) {
-        uniqueFrequency += entry.second; // You can modify this based on requirement
+int atMostKDistinct(string &S, int K) {
+    unordered_map<char, int> charCount;
+    int left = 0, right = 0, count = 0, distinct = 0;
+
+    for (right = 0; right < S.size(); ++right) {
+        charCount[S[right]]++;
+        if (charCount[S[right]] == 1) { // New distinct character
+            distinct++;
+        }
+
+        while (distinct > K) {
+            charCount[S[left]]--;
+            if (charCount[S[left]] == 0) { // No more of this character
+                distinct--;
+            }
+            left++;
+        }
+
+        // Every time we reach here, the window [left, right] has at most K distinct characters
+        count += right - left + 1;
     }
 
-    return uniqueFrequency;
+    return count;
+}
+
+int substrCount(string S, int K) {
+    return atMostKDistinct(S, K) - atMostKDistinct(S, K - 1);
 }
 
 int main() {
-    string s;
-    int k;
+    string S;
+    int K;
     
-    cout << "Enter string: ";
-    cin >> s;
-    cout << "Enter length k: ";
-    cin >> k;
-    
-    int result = calculateFrequency(s, k);
-    cout << "Output: " << result << endl;
+    cout << "Enter string S: ";
+    cin >> S;
+    cout << "Enter integer K: ";
+    cin >> K;
+
+    cout << "Number of substrings with exactly " << K << " distinct characters: " << substrCount(S, K) << endl;
 
     return 0;
 }
