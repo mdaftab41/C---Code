102304a59835b68abfcb3aad3e05833f48d2f1a2
mdaftab41 <mdaftabalam49041@gmail.com>
2024-10-14 22:29:39 +0530

    FinalCommit

--- String/firstOccuranceOfWord.cpp
+++ String/firstOccuranceOfWord.cpp
@@ -2,37 +2,55 @@
 #include <string>
 using namespace std;
 
+// int strStr(string haystack, string needle) {
+//     int n = haystack.size();
+//     int m = needle.size();
+    
+//     if (m > n) {
+//         return -1;
+//     }
+    
+//     if (n == m && needle == haystack) {
+//         return 0;
+//     }
+    
+//     string temp = "";
+//     for (int i = 0; i < n; i++) {
+//         if (temp != needle) {
+//             temp += haystack[i];
+//             if (temp.size() > m) {
+//                 temp.erase(temp.begin(), temp.begin() + 1);
+//             }
+//         }
+        
+//         // Debugging output to track current temp and index
+//         cout << "Current temp: " << temp << " at index " << i << endl;
+
+//         if (temp == needle && temp.size() == m) {
+//             return i - m + 1;  // Adjusted to return correct starting index
+//         }
+//     }
+    
+//     return -1;
+// }
+
+
+
 int strStr(string haystack, string needle) {
-    int n = haystack.size();
-    int m = needle.size();
-    
-    if (m > n) {
-        return -1;
-    }
-    
-    if (n == m && needle == haystack) {
-        return 0;
-    }
-    
-    string temp = "";
-    for (int i = 0; i < n; i++) {
-        if (temp != needle) {
-            temp += haystack[i];
-            if (temp.size() > m) {
-                temp.erase(temp.begin(), temp.begin() + 1);
+        int n = haystack.size();
+        int m = needle.size();
+        
+        if (m > n) return -1;  // If needle is longer than haystack
+        if (needle == haystack) return 0;  // If both strings are identical
+
+        for (int i = 0; i <= n - m; i++) {  // Loop until there's enough space for needle
+            if (haystack.substr(i, m) == needle) {
+                return i;  // Found the needle in haystack
             }
         }
         
-        // Debugging output to track current temp and index
-        cout << "Current temp: " << temp << " at index " << i << endl;
-
-        if (temp == needle && temp.size() == m) {
-            return i - m + 1;  // Adjusted to return correct starting index
-        }
+        return -1;  // Needle not found
     }
-    
-    return -1;
-}
 
 int main() {
     string haystack, needle;
