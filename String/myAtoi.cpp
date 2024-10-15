9b214e34ccf86ac15a684ca885d6ca60d0c2d34c
mdaftab41 <mdaftabalam49041@gmail.com>
2024-10-16 01:25:50 +0530

    Adding Question

--- String/myAtoi.cpp
+++ String/myAtoi.cpp
@@ -5,7 +5,7 @@
 using namespace std;   
 
 
-Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer.
+/*Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer.
 
 The algorithm for myAtoi(string s) is as follows:
 
@@ -13,7 +13,7 @@ Whitespace: Ignore any leading whitespace (" ").
 Signedness: Determine the sign by checking if the next character is '-' or '+', assuming positivity is neither present.
 Conversion: Read the integer by skipping leading zeros until a non-digit character is encountered or the end of the string is reached. If no digits were read, then the result is 0.
 Rounding: If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then round the integer to remain in the range. Specifically, integers less than -231 should be rounded to -231, and integers greater than 231 - 1 should be rounded to 231 - 1.
-Return the integer as the final result.
+Return the integer as the final result.*/
 
 int myAtoi(string s) {   
     long long int ans = 0;
