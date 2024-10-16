e16b464e8da5c431ee1250d11e4084315dd4906a
mdaftab41 <mdaftabalam49041@gmail.com>
2024-10-17 02:11:44 +0530

    subSet

---
+++ BitsWise/subset.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std  ;
+
+vector<vector<int>>subSet(vector<int>&nums){
+    int n=nums.size() ;
+    vector<vector<int>>ans;
+    for( int i=0 ; i<(1<<n) ; i++){
+        vector<int>result;
+        for( int j=0 ; j<n ; j++){
+            if( i & (1<<j)){
+                result.push_back(nums[j]) ;
+            }
+        }
+        ans.push_back(result) ;
+    }
+    return ans ;
+}
+
+int main(){
+    int m ; 
+    cout<<"Enter the array Size"<<endl;
+    cin>>m ;
+    cout<<"Enter the Array"<<endl;
+    vector<int>arr(m);
+    for( int i=0 ; i<m; i++){
+    cin>>arr[i] ;
+    }
+    vector<vector<int>>ans=subSet(arr) ;
+    for( auto it:ans){
+        for(auto jt :it){
+              cout << jt << " ";
+        }
+        cout<<endl;
+    }
+  return  0;
+}
