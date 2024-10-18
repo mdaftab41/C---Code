57140c9879991dfc011301035a5ff20874f6448b
mdaftab41 <mdaftabalam49041@gmail.com>
2024-10-18 22:54:36 +0530

    string

--- Map/findAllDuplicateOfString.cpp
+++ Map/findAllDuplicateOfString.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     string str;
-    cout<<"Enter the string "<<endl;
+    cout<<"Enter the string  "<<endl;
     getline(cin , str) ;
     int len=str.length();
     unordered_map<char , int>mp;
