169c4a21d65868730abc7ac6dbd8610892f661b0
mdaftab41 <mdaftabalam49041@gmail.com>
2024-10-28 23:41:14 +0530

    poweroftwo

--- Striver’s-TCS/Number/isPowerOfTwo.cpp
+++ Striver’s-TCS/Number/isPowerOfTwo.cpp
@@ -1,14 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+// bool isPoweroftwo( int n){
+//     int ans=1 ;
+//     for(int i=0 ; i<=30 ; i++){
+//         if(ans==n){
+//             return true ;
+//         }
+//         if(ans<INT_MAX/2){
+//             ans=ans*2 ;
+//         }
+//     }
+//     return false ;
+// }
+
+
 bool isPoweroftwo( int n){
-    int ans=1 ;
+     
     for(int i=0 ; i<=30 ; i++){
+        int ans=pow(2 , i) ;
         if(ans==n){
             return true ;
-        }
-        if(ans<INT_MAX/2){
-            ans=ans*2 ;
         }
     }
     return false ;
