ff7d90ae006be015308b8f26bfdd4c6f1d53e47c
mdaftab41 <mdaftabalam49041@gmail.com>
2024-11-10 17:57:20 +0530

    Decimal to Binar

---
+++ BitsWise/convertDecimalToBimary.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string DecToBinary( int n){
+    string str= "" ;
+    while(n != 0){
+        if(n%2==1){
+            str+="1" ;
+        }else{
+            str+="0" ;
+        }
+        n=n/2 ;
+    }
+    return str ;
+}
+
+
+int main(){
+    int n ;
+    cout<<"Enter the va;ue of n " <<endl;
+    cin>> n ;
+    string temp=DecToBinary(n) ;
+    cout<<temp ;
+    return 0 ;
+}
