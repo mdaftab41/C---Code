695e388ed6c0574159c8af8c2645840a064196f0
mdaftab41 <mdaftabalam49041@gmail.com>
2024-11-24 02:03:14 +0530

    optimal code

--- leetCode/3Sum.cpp
+++ leetCode/3Sum.cpp
@@ -5,24 +5,64 @@
 
 using namespace std;
 
+
+
 vector<vector<int>> threeSum(vector<int>& nums) {
-    int n = nums.size();
-    set<vector<int>> st;
-    for (int i = 0; i < n; i++) {
-        set<int> hashset;
-        for (int j = i + 1; j < n; j++) {
-            int third = -(nums[i] + nums[j]);
-            if (hashset.find(third) != hashset.end()) {
-                vector<int> temp = {nums[i], nums[j], third};
-                sort(temp.begin(), temp.end());
-                st.insert(temp);
+     int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end()); // Sort the array to use two-pointer approach
+        
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])  // Skip duplicate elements for the first number
+                continue;
+            
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    vector<int> temp = {nums[i], nums[j], nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    
+                    // Skip duplicates for the second number
+                    while (j < k && nums[j] == nums[j - 1]) j++; // Fixed infinite loop issue
+                    // Skip duplicates for the third number
+                    while (j < k && nums[k] == nums[k + 1]) k--; // Fixed infinite loop issue
+                }
             }
-            hashset.insert(nums[j]);
         }
-    }
-    vector<vector<int>> ans(st.begin(), st.end());
-    return ans;
+        return ans; 
 }
+
+
+
+
+
+// vector<vector<int>> threeSum(vector<int>& nums) {
+//     int n = nums.size();
+//     set<vector<int>> st;
+//     for (int i = 0; i < n; i++) {
+//         set<int> hashset;
+//         for (int j = i + 1; j < n; j++) {
+//             int third = -(nums[i] + nums[j]);
+//             if (hashset.find(third) != hashset.end()) {
+//                 vector<int> temp = {nums[i], nums[j], third};
+//                 sort(temp.begin(), temp.end());
+//                 st.insert(temp);
+//             }
+//             hashset.insert(nums[j]);
+//         }
+//     }
+//     vector<vector<int>> ans(st.begin(), st.end());
+//     return ans;
+// }
 
 int main() {
     // Example input
